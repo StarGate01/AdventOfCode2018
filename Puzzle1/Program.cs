c4e3e834858ec05d3d5b6d5edd272cfc76a77488
Christoph Honal <christoph.honal@web.de>
2018-12-03 02:23:23 +0100

    Add puzzles of day 1

--- Puzzle1/Program.cs
+++ Puzzle1/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Puzzle1
 {
+
+    // https://adventofcode.com/2018/day/1
+
     class Program
     {
+
         static void Main(string[] args)
         {
+            Console.WriteLine(File.ReadLines("input/input.txt")
+                .Select(p => Int32.TryParse(p, out int offs) ? offs : 0)
+                .Aggregate((p, q) => p + q));
         }
+
     }
+
 }
---
+++ Puzzle2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Puzzle2
+{
+
+    // https://adventofcode.com/2018/day/1
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var seen = new List<int>();
+            var result = 0;
+            var list = File.ReadLines("input/input.txt")
+                .Select(p => Int32.TryParse(p, out int offs) ? offs : 0)
+                .ToList();
+
+            while (true)
+            {
+                foreach (var p in list)
+                {
+                    result += p;
+                    if (seen.Contains(result))
+                    {
+                        Console.WriteLine(result);
+                        Environment.Exit(0);
+                    }
+                    seen.Add(result);
+                }
+            }
+        }
+
+    }
+
+}
